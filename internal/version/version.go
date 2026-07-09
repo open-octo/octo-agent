02803a4d9424bb407d90881e71a7546594cb189c
Roy <leihaibo1992@gmail.com>
2026-07-09 14:51:16 +0800

    chore: bump version to 1.10.4 (#1277)

--- internal/version/version.go
+++ internal/version/version.go
@@ -11,7 +11,7 @@ import (
 )
 
 // Version is the SemVer string for this build. Overridden via -ldflags.
-var Version = "1.10.3"
+var Version = "1.10.4"
 
 // Commit is the short git SHA for this build. Overridden via -ldflags.
 // Empty in local `go build` invocations.
